a4452f1ffd4d79a81f49b099b3d1ba78e02dc514
Josh Johnson <josh@twotoasters.com>
2015-04-22 14:51:57 -0400

    added updated design by @aditshukla

---
+++ js/sticky.js
@@ -0,0 +1,21 @@
+// Demo by Aurelio De Rosa (@AurelioDeRosa)
+// See article: http://www.sitepoint.com/css-position-sticky-introduction-polyfills/
+
+var menu = document.querySelector('.menu');
+var menuPosition = menu.getBoundingClientRect();
+var placeholder = document.createElement('div');
+placeholder.style.width = menuPosition.width + 'px';
+placeholder.style.height = menuPosition.height + 'px';
+var isAdded = false;
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset >= menuPosition.top && !isAdded) {
+        menu.classList.add('sticky');
+        menu.parentNode.insertBefore(placeholder, menu);
+        isAdded = true;
+    } else if (window.pageYOffset < menuPosition.top && isAdded) {
+        menu.classList.remove('sticky');
+        menu.parentNode.removeChild(placeholder);
+        isAdded = false;
+    }
+});
